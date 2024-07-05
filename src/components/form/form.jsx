38406c125ea70836287a4d1cddc4ed474b3bde9a
EternumArt <sli@sste.ru>
2024-07-05 15:39:45 +0300

    form re-create

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,78 +1,128 @@
 import React, { useState } from "react";
-//import { Fieldset } from "./fieldset/fieldset";
 import styles from "./form.module.css";
-//import { Button } from "../button/button";
 import { checkFio, checkEmail, checkPassword } from "../../utils/validation";
+import cn from "../../utils/classNames";
+
+const Error = ({ props }) => {
+	if (props.state) {
+		return (
+			<span className={styles.formError} id={props.id}>
+				{props.text}
+			</span>
+		);
+	}
+};
 
 export const Form = () => {
-	const Fieldset = ({ props }) => {
-		const [error, setError] = useState(false);
+	const [fioError, setFioError] = useState(false);
+	const [loginError, setLoginError] = useState(false);
+	const [passwordError, setPasswordError] = useState(false);
+	const [buttonDisabled, setButtonDisabled] = useState(true);
+	const [, updateState] = React.useState();
+	const forceUpdate = React.useCallback(() => updateState({}), []);
 
-		const validate = (inputEvent) => {
-			switch (inputEvent.currentTarget.type) {
-				case "text": {
-					const result = checkFio(inputEvent.currentTarget);
-					if (result !== false) {
-						inputEvent.currentTarget.value = result;
-						setError(false);
-					} else {
-						setError(true);
-					}
-					break;
-				}
-				case "email": {
-					const result = checkEmail(inputEvent.currentTarget);
-					result !== false ? setError(false) : setError(true);
-					break;
-				}
-				case "password": {
-					const result = checkPassword(inputEvent.currentTarget);
-					result !== false ? setError(false) : setError(true);
-					break;
-				}
-				default: {
-					break;
-				}
+	const handlerSubmit = (evt) => {
+		evt.preventDefault();
+		if (fioError || loginError || passwordError) {
+			return;
+		}
+		const inputs = evt.currentTarget.querySelectorAll("input");
+		const formData = new FormData();
+		for (const input of inputs) {
+			if (input.type !== "submit") {
+				formData.append(input.id, input.value);
 			}
-		};
+		}
 
-		return (
-			<fieldset className={styles.formInputWrapper}>
-				<input type={props.type} className={styles.formInput} placeholder={props.placeholder} id={props.id} minLength={props.minLength} required onChange={validate} />
-				{error ? (
-					<span className={styles.formError} id={props.errorType}>
-						{props.errorText}
-					</span>
-				) : (
-					""
-				)}
-			</fieldset>
-		);
+		console.log(...formData);
 	};
 
-	const Button = ({ props }) => {
-		const [buttonDisabled, setButtonDisabled] = useState(true);
+	const validation = (input) => {
+		if (input.type === "text") {
+			const fioResult = checkFio(input);
+			if (fioResult) {
+				setFioError(false);
+				input.value = fioResult;
+			} else {
+				setFioError(true);
+			}
+		}
+		if (input.type === "email") {
+			const emailResult = checkEmail(input);
+			if (emailResult) {
+				setLoginError(false);
+			} else {
+				setLoginError(true);
+			}
+		}
+		if (input.type === "password") {
+			const passwordResult = checkPassword(input);
+			if (passwordResult) {
+				setPasswordError(false);
+			} else {
+				setPasswordError(true);
+			}
+		}
 
-		let style;
-		buttonDisabled ? (style = { background: "rgba(0, 0, 0, 0.5)", pointerEvents: "none" }) : (style = null);
-		return <input className={`${styles.button} ${styles.buttonForm}`} style={style} id={props.id} value={props.value} type={props.type} />;
+		console.log(fioError, loginError, passwordError);
+
+		checkButton([fioError, loginError, passwordError]);
+	};
+
+	const checkButton = (array) => {
+		const result = array.find((boolean) => {
+			return boolean === true;
+		});
+
+		result ? setButtonDisabled(true) : setButtonDisabled(false);
+		forceUpdate();
 	};
 
 	return (
-		<form className={`${styles.form} ${styles.formRegistration}`} method='post' onSubmit={console.log("submit")} noValidate>
-			<Fieldset
-				props={{
-					type: "text",
-					placeholder: "И. О. Фамилия",
-					id: "fio",
-					minLength: "4",
-					errorType: "error-name",
-					errorText: "Не валидное имя",
-				}}
-			/>
-			<Fieldset props={{ type: "email", placeholder: "E-mail", id: "login", minLength: "4", errorType: "error-email", errorText: "Не валидный E-mail" }} />
-			<Fieldset props={{ type: "password", placeholder: "Пароль", id: "password", minLength: "8", errorType: "error-password", errorText: "Слабый пароль" }} />
-			<Button props={{ id: "registration-btn", value: "Зарегистрироваться", type: "submit" }} />
+		<form className={cn([styles.form, styles.formRegistration])} noValidate onSubmit={handlerSubmit}>
+			<fieldset className={styles.formInputWrapper}>
+				<input
+					type='text'
+					className={styles.formInput}
+					placeholder='И. О. Фамилия'
+					id='fio'
+					minLength='4'
+					required
+					onChange={(evt) => {
+						validation(evt.currentTarget);
+					}}
+				/>
+				<Error props={{ state: fioError, id: "fio", text: "Не валидное имя" }} />
+			</fieldset>
+			<fieldset className={styles.formInputWrapper}>
+				<input
+					type='email'
+					className={styles.formInput}
+					placeholder='Рабочая почта'
+					id='login'
+					required
+					onChange={(evt) => {
+						validation(evt.currentTarget);
+					}}
+				/>
+				<Error props={{ state: loginError, id: "login", text: "Не валидный E-mail" }} />
+			</fieldset>
+			<fieldset className={styles.formInputWrapper}>
+				<input
+					type='password'
+					className={styles.formInput}
+					placeholder='Пароль'
+					id='password'
+					minLength='8'
+					required
+					onChange={(evt) => {
+						validation(evt.currentTarget);
+					}}
+				/>
+				<Error props={{ state: passwordError, id: "password", text: "Слабый пароль" }} />
+			</fieldset>
+
+			{buttonDisabled ? <input type='submit' className={cn([styles.button, styles.buttonForm, styles.buttonDisabled])} id='registration-btn' value='Зарегистрироваться' /> : <input type='submit' className={cn([styles.button, styles.buttonForm])} id='registration-btn' value='Зарегистрироваться' />}
 		</form>
 	);
 };
---
+++ src/utils/classNames.jsx
@@ -0,0 +1,3 @@
+export default function cn (array) {
+    return array.join(" ")
+}
